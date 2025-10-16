db85bc0fd69bc754d4904172cf944e62fccb3e8d
ifranc-r <ifranc-r@student.42.fr>
2025-10-16 18:22:29 +0200

    add  whitelist in the .env envirement

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
-$dsn = 'mysql:host=db;dbname=camagru;charset=utf8mb4';
+$dsn = 'mysql:host='. $_ENV['DB_HOST'] .';dbname='. $_ENV['DB_NAME'] .';charset=utf8mb4';
 $options = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   PDO::ATTR_EMULATE_PREPARES => false,
 ];
-$pdo = new PDO($dsn, 'cam', 'cam', $options);
+$pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,5 @@
 <?php
-echo "stack ready";
+require __DIR__ . '/../db.php';
+
+var_dump($_ENV['DB_USER'] ?? null);
+var_dump($_ENV['DB_PASS'] ?? null);
